eee3777c40729e151e2d73ce309ee874ce9b5864
maddrake220 <jwchoi4118@gmail.com>
2021-12-14 16:56:21 +0900

    feat: onload only each image

--- main.js
+++ main.js
@@ -1,72 +1,99 @@
-const slider = document.querySelector(".slider");
+const customSlider = (target, option) => {
+  /* 이미지 로드 여부 */
+  // 이미지에만 onload 하는 방법 -> 퍼포먼스 향상
+  // async await 대체 가능
+  const toBeLoaded = document.querySelectorAll(`${target} img`);
+  let loadedImages = 0;
+  toBeLoaded.forEach((image) => {
+    image.onload = () => {
+      loadedImages++;
+      if (loadedImages === toBeLoaded.length) {
+        innerName(target, option);
+      } else {
+        return;
+      }
+    };
+  });
 
-// add parent div kindWrap
-const parent = slider.parentNode;
-const kindWrap = document.createElement("div");
-kindWrap.classList.add("kind_wrap");
-parent.replaceChild(kindWrap, slider);
-kindWrap.appendChild(slider);
-// add parent div kind_slider
-const parent2 = slider.parentNode;
-const kind_slider = document.createElement("div");
-kind_slider.classList.add("kind_slider");
-parent2.replaceChild(kind_slider, slider);
-kind_slider.appendChild(slider);
-// add child div moveButton
-const moveButton = document.createElement("div");
-moveButton.classList.add("arrow");
-kindWrap.appendChild(moveButton);
-// add child a prev and next
-const prev = document.createElement("a");
-prev.classList.add("prev");
-prev.innerText = "이전";
-prev.href = "";
-moveButton.appendChild(prev);
-const next = document.createElement("a");
-next.classList.add("next");
-next.innerText = "다음";
-next.href = "";
-moveButton.appendChild(next);
+  // window.onload 방법 => window 모든 객체를 체크해서 퍼포먼스 낮음
+  // window.onload = (function (target, option) {
+  //   return () => {
+  //     innerName(target, option);
+  //   };
+  // })(target, option);
 
-const slideLis = slider.querySelectorAll("li");
-let duration = "1000ms";
-const node = slideLis[0].cloneNode(true);
-const node2 = slideLis[slideLis.length - 1].cloneNode(true);
-slider.appendChild(node);
-slider.insertBefore(node2, slideLis[0]);
-const liWidth = slideLis[0].clientWidth;
-const newslideLis = slider.querySelectorAll("li");
-const sliderWidth = liWidth * newslideLis.length;
-slider.style.width = sliderWidth + "px";
-let focus = 1;
-let moveDist = -liWidth;
-slider.style.transform = `translateX(${moveDist}px)`;
-moveButton.addEventListener("click", (value) => {
-  value.preventDefault();
+  /* READY DOM */
+  function innerName(target, option) {
+    const slider = document.querySelector(target);
+    // add parent div kindWrap
+    const parent = slider.parentNode;
+    const kindWrap = document.createElement("div");
+    kindWrap.classList.add("kind_wrap");
+    parent.replaceChild(kindWrap, slider);
+    kindWrap.appendChild(slider);
+    // add parent div kind_slider
+    const parent2 = slider.parentNode;
+    const kind_slider = document.createElement("div");
+    kind_slider.classList.add("kind_slider");
+    parent2.replaceChild(kind_slider, slider);
+    kind_slider.appendChild(slider);
+    // add child div moveButton
+    const moveButton = document.createElement("div");
+    moveButton.classList.add("arrow");
+    kindWrap.appendChild(moveButton);
+    // add child a prev and next
+    const prev = document.createElement("a");
+    prev.classList.add("prev");
+    prev.innerText = "이전";
+    prev.href = "";
+    moveButton.appendChild(prev);
+    const next = document.createElement("a");
+    next.classList.add("next");
+    next.innerText = "다음";
+    next.href = "";
+    moveButton.appendChild(next);
 
-  if (value.target.className === "next") {
-    focus++;
-    moveDist += -liWidth;
-  } else {
-    focus--;
-    moveDist += liWidth;
-  }
-  slider.style.transition = `transform ${duration}`;
-  slider.style.transform = `translateX(${moveDist}px)`;
-  if (focus === slideLis.length + 1) {
-    setTimeout(() => {
-      moveDist = -liWidth;
-      slider.style.transition = "none";
+    const slideLis = slider.querySelectorAll("li");
+    let duration = option.speed;
+    const node = slideLis[0].cloneNode(true);
+    const node2 = slideLis[slideLis.length - 1].cloneNode(true);
+    slider.appendChild(node);
+    slider.insertBefore(node2, slideLis[0]);
+    const liWidth = slideLis[0].clientWidth;
+    const newslideLis = slider.querySelectorAll("li");
+    const sliderWidth = liWidth * newslideLis.length;
+    slider.style.width = sliderWidth + "px";
+    let focus = 1;
+    let moveDist = -liWidth;
+    slider.style.transform = `translateX(${moveDist}px)`;
+    moveButton.addEventListener("click", (value) => {
+      value.preventDefault();
+
+      if (value.target.className === "next") {
+        focus++;
+        moveDist += -liWidth;
+      } else {
+        focus--;
+        moveDist += liWidth;
+      }
+      slider.style.transition = `transform ${duration}ms`;
       slider.style.transform = `translateX(${moveDist}px)`;
-      focus = 1;
-    }, 1000);
+      if (focus === slideLis.length + 1) {
+        setTimeout(() => {
+          moveDist = -liWidth;
+          slider.style.transition = "none";
+          slider.style.transform = `translateX(${moveDist}px)`;
+          focus = 1;
+        }, duration);
+      }
+      if (focus === 0) {
+        setTimeout(() => {
+          moveDist = -liWidth * slideLis.length;
+          slider.style.transition = "none";
+          slider.style.transform = `translateX(${moveDist}px)`;
+          focus = slideLis.length;
+        }, duration);
+      }
+    });
   }
-  if (focus === 0) {
-    setTimeout(() => {
-      moveDist = -liWidth * slideLis.length;
-      slider.style.transition = "none";
-      slider.style.transform = `translateX(${moveDist}px)`;
-      focus = slideLis.length;
-    }, 1000);
-  }
-});
+};
